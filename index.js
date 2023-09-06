aa9c374f17cabf79b70ef07cd3d8311f5879a686
Temuu-jin <temucin.d.k@gmail.com>
2023-09-06 21:53:04 +0100

    Finish ToDo, Add Comments for Learning

--- index.js
+++ index.js
@@ -1,122 +1,93 @@
+import fs from 'node:fs';
 import fetch from 'node-fetch';
 import { parse } from 'node-html-parser';
 
-const response = await fetch(
-  'https://memegen-link-examples-upleveled.netlify.app/',
-);
-const body = await response.text();
+// Declare websiteUrl variable
+const websiteUrl = 'https://memegen-link-examples-upleveled.netlify.app/'; // Replace with the actual website URL
 
-const parsedBody = parse(body);
+fetch(websiteUrl)
+  .then((response) => {
+    if (response.ok) {
+      return response.text();
+    } else {
+      throw new Error(`Failed to fetch the website: ${response.statusText}`);
+    }
+  })
+  .then((html) => {
+    // Parse the HTML content
+    const root = parse(html);
 
-const images = parsedBody.querySelectorAll('img');
+    // select all <img> elements and extract their src attributes
+    const imgElements = root.querySelectorAll('img');
+    const imgSrcList = imgElements
+      .map((img) => img.getAttribute('src'))
+      .filter((src) => src);
 
-images.forEach((img) => {
-  img.src;
-});
+    // limit the number of images to download
+    const maxImagesToDownload = 10;
 
-console.log(images);
+    // maxImagesToDownload Loop and Download (use filePath and imageUrl)
+    for (
+      let index = 0;
+      index < imgSrcList.length && index < maxImagesToDownload;
+      index++
+    ) {
+      const src = imgSrcList[index];
 
+      /*
+      every number under 10 gets a 0 before its index+1. if 10 its just index+1
+      */
+      const fileName = `img${index + 1 < 10 ? `0${index + 1}` : index + 1}.jpg`;
+      /*
+      Declare constants for downloading so the code looks neat. used for fs.writeFileSync
+      */
+      const filePath = `./memes/${fileName}`; // where to save
+      const imageUrl = new URL(src, websiteUrl); // create an absolute URL
+
+      fetch(imageUrl.href)
+        .then((response) => {
+          if (response.ok) {
+            return response.buffer(); // return the image data as a .buffer
+          } else {
+            throw new Error(`Failed to download image: ${response.statusText}`);
+          }
+        })
+        .then((imageBuffer) => {
+          fs.writeFileSync(filePath, imageBuffer); // Save images to filePath
+          console.log(`Downloaded and saved ${fileName}`);
+        })
+        .catch((error) => {
+          console.error(`Error downloading image: ${error.message}`);
+        });
+    }
+  })
+  .catch((error) => {
+    console.error('Error:', error.message);
+  });
 /*
-
 Findings Timeline:
 
-Steps are:
-Fetch HTML Code --- Library found: node-fetch
-Parse HTML to find image links --- Library found: node-html-parser
-Download images
-Rename each to desired unique name
-save to specific directory
+- Steps are:
+  Fetch HTML Code --- Library found: node-fetch
+  Parse HTML to find image links --- Library found: node-html-parser
+  Download images
+  Rename each to desired unique name
+  save to specific directory
 
+Fetch works in a way that it returns a "Promise" that represents the result of this asynchronous operation. The Promise will be in a pending state until the HTTP request is complete.
 
---- Implemented Fetch Library:
-const response = await fetch('lazytopastasojustmemelink.netlify.app',);
+.then((response)=>{}) always has a response and a callback function to write what to do with this result from fetch
 
-const body = await response.text();
+use if(response.ok) else for error handling
 
-console.log(parsedBody);
+.then((html)=>) here we use parse(html) to  parse and assign the raw response text to root so we can use querySelectorAll('img') and select all <img>
 
-return was HTML Body Code, WORKING! :D
+imgElements.forEach((img)=>{}) loops through object array and pushes the src on an array imgSrcList
 
---- Implement Parse Library:
-const response = await fetch(
-  'https://memegen-link-examples-upleveled.netlify.app/',
-);
-const body = await response.text();
+We now have an Array with all the src urls in order.
 
-const parsedBody = parse(body);
+for limiting to 10 i had to change from forEach() to a for(limit){fetch().then().catch()} loop
 
-console.log(parsedBody);
-
-return:
-temu@LAPTOP-Q55UNDJC MINGW64 /d/Projects/meme-scraper (main)
-$ node index.js
-<ref *1> HTMLElement {
-  parentNode: null,
-  childNodes: [
-    TextNode {
-      parentNode: [Circular *1],
-      childNodes: [],
-      nodeType: 3,
-      _rawText: '<!DOCTYPE html>\n'
-    },
-    HTMLElement {
-      parentNode: [Circular *1],
-      childNodes: [Array],
-      rawAttrs: '',
-      voidTag: [VoidTag],
-      nodeType: 1,
-      rawTagName: 'html',
-      id: '',
-      _parseOptions: {},
-      classList: [DOMTokenList]
-    },
-    TextNode {
-      parentNode: [Circular *1],
-      childNodes: [],
-      nodeType: 3,
-      _rawText: '\n'
-    }
-  ],
-  rawAttrs: '',
-  voidTag: VoidTag {
-    addClosingSlash: false,
-    voidTags: Set(14) {
-      'area',
-      'base',
-      'br',
-      'col',
-      'embed',
-      'hr',
-      'img',
-      'input',
-      'link',
-      'meta',
-      'param',
-      'source',
-      'track',
-      'wbr'
-    }
-  },
-  nodeType: 1,
-  rawTagName: null,
-  id: '',
-  _parseOptions: {},
-  classList: DOMTokenList {
-    _set: Set(0) {},
-    _afterUpdate: [Function (anonymous)]
-  }
-}
-
-What do I do with this info parsed? IDK
-Looks like 'source' is one of the attributes I have to use to loop through the 'src' string which I need for downloading the img's
-
-Can I create an Array or Objects that loops through all the src and saves them in numerical order (img01, img02, etc.)
-Each object has attributes. Name: img01, Link: parsed string
-
-These will be used later on for Downloading from Link and then saving with correct Name
-
-Dont know yet how to download and save under specific name so I might be wrong
-
-BUT, the Parser Library is installed correctly and works!
-
-*/
+JavaScript provides error.message to handle errors. when error is thrown a error object is generated with info about error
+response.statusText is for error handling from the node-fetch library
+ */
